8a2acb09baa3ad6a44f5183165c47a3e77832da1
Abdulluh.X 🇸🇦 <225096852@student.kfu.edu.sa>
2026-05-19 04:36:01 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
 # index.py
+import os
+import sys
+
+# إجبار سيرفر فيرسل على قراءة الجذر الرئيسي لكي يرى مجلد vipertls بدون مشاكل
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from vipertls.solver.server import app
 
-# تفعيل لوحة التحكم التفاعلية docs إجبارياً على السيرفر السحابي
+# تفعيل لوحة التحكم التفاعلية docs إجبارياً
 app.docs_url = "/docs"
 app.openapi_url = "/openapi.json"
 app.setup_openapi()
